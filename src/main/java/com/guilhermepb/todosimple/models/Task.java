9e99b6c0fb5b5a76a85fbe990ecf504be120a36b
Guilherme P Borges da Silva <guilherme.silva645@fatec.sp.gov.br>
2024-09-05 11:46:33 -0300

    Create Task Model

---
+++ src/main/java/com/guilhermepb/todosimple/models/Task.java
@@ -0,0 +1,118 @@
+package com.guilhermepb.todosimple.models;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@Table(name = Task.TABLE_NAME) //table for database
+public class Task {
+
+    public static final String TABLE_NAME = "task";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //id is random (User id)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @ManyToOne //Many tasks for one user
+    @JoinColumn(name = "user_id", nullable = false, updatable = false) //this is for make reference of "user_id"
+    private User user;                                                 //"user_id" of: (folder)models --> User
+
+    @Column(name = "description", length = 255, nullable = false)
+    @NotBlank                 //@NotNull + @NotEmpty == @NotBlank
+    @Size(min = 1, max = 255) //limits for description
+    private String description;
+
+    public Task() {
+    }
+
+    //constructor
+    public Task(Long id, User user, String description) {
+        this.id = id;
+        this.user = user;
+        this.description = description;
+    }
+
+    //getters and setters
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public @NotBlank @Size(min = 1, max = 255) String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(@NotBlank @Size(min = 1, max = 255) String description) {
+        this.description = description;
+    }
+
+    public Task id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public Task user(User user) {
+        setUser(user);
+        return this;
+    }
+
+    public Task description(String description) {
+        setDescription(description);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object object) { //this 'equals' is to ensure:
+        if (object == this){               // this.(id, user and description) == other.(id, username and password)
+            return true;
+        }
+        if (!(object instanceof Task)){
+            return false;
+        }
+        if (object == null){
+            return false;
+        }
+        Task other = (Task) object;
+        if (this.id == null){
+            if (other.id != null){
+                return false;
+            }
+            else if (!this.id.equals(other.id)){
+                return false;
+            }
+        }
+        return Objects.equals(this.id, other.id) && Objects.equals(this.user, other.user) &&
+                Objects.equals(this.description, other.description);
+
+    }
+
+    @Override
+    public int hashCode() { //storage and recuperation of objects, each object had a hashcode
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+}
--- src/main/java/com/guilhermepb/todosimple/models/User.java
+++ src/main/java/com/guilhermepb/todosimple/models/User.java
@@ -1,7 +1,7 @@
 package com.guilhermepb.todosimple.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.scheduling.config.Task;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -20,7 +20,7 @@ public class User {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //id is random
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //id is random (User id)
     @Column(name = "id", unique = true)
     private Long id;
 
@@ -30,19 +30,21 @@ public class User {
     @Size(groups = CreateUser.class, min = 2, max = 100) //limits of length
     private String username;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) //write_only: front side don't have access of password
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) //write_only:front side don't have access of password
     @Column(name = "password", length = 60, nullable = false) //limits
     @NotNull(groups = {CreateUser.class, UpdateUser.class}) // password can be created and updated
     @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
     @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60) //limits of length
     private String password;
 
-    //private List<Task> tasks = new ArrayList<Task>();
+    @OneToMany(mappedBy = "user") // One user can have many tasks
+    private List<Task> tasks = new ArrayList<Task>(); //tasks list
 
     public User(){
     }
 
-    public User(Long id, String username, String password) { //constructor
+    //constructor
+    public User(Long id, String username, String password) {
         this.id = id;
         this.username = username;
         this.password = password;
@@ -58,24 +60,41 @@ public class User {
         this.id = id;
     }
 
-    public @NotNull(groups = CreateUser.class) @NotEmpty(groups = CreateUser.class) @Size(groups = CreateUser.class, min = 2, max = 100) String getUsername() {
+    public @NotNull(groups = CreateUser.class)
+            @NotEmpty(groups = CreateUser.class) @Size(groups = CreateUser.class, min = 2,
+            max = 100) String getUsername() {
         return this.username;
     }
 
-    public void setUsername(@NotNull(groups = CreateUser.class) @NotEmpty(groups = CreateUser.class) @Size(groups = CreateUser.class, min = 2, max = 100) String username) {
+    public void setUsername(@NotNull(groups = CreateUser.class)
+                            @NotEmpty(groups = CreateUser.class) @Size(groups = CreateUser.class, min = 2,
+                            max = 100) String username) {
         this.username = username;
     }
 
-    public @NotNull(groups = {CreateUser.class, UpdateUser.class}) @NotEmpty(groups = {CreateUser.class, UpdateUser.class}) @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60) String getPassword() {
+    public @NotNull(groups = {CreateUser.class, UpdateUser.class})
+                              @NotEmpty(groups = {CreateUser.class, UpdateUser.class}) @Size(groups =
+                              {CreateUser.class, UpdateUser.class}, min = 8, max = 60) String getPassword() {
         return this.password;
     }
 
-    public void setPassword(@NotNull(groups = {CreateUser.class, UpdateUser.class}) @NotEmpty(groups = {CreateUser.class, UpdateUser.class}) @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60) String password) {
+    public void setPassword(@NotNull(groups = {CreateUser.class, UpdateUser.class})
+                            @NotEmpty(groups = {CreateUser.class, UpdateUser.class}) @Size(groups =
+                            {CreateUser.class, UpdateUser.class}, min = 8, max = 60) String password) {
         this.password = password;
     }
 
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    //this 'equals' is to ensure: this.(id, username and password) == other.(id, username and password)
     @Override
-    public boolean equals(Object object) { //this 'equals' is to ensure: this.(id, username and password) == other.(id, username and password)
+    public boolean equals(Object object) {
         if (object == this){
             return true;
         }
@@ -94,9 +113,11 @@ public class User {
                 return false;
             }
         }
-        return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username) && Objects.equals(this.password, other.password);
+        return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username)
+                && Objects.equals(this.password, other.password);
     }
 
+    //hashCode
     @Override
     public int hashCode() { //storage and recuperation of objects, each object had a hashcode
         final int prime = 31;
@@ -104,4 +125,5 @@ public class User {
         result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
         return result;
     }
+
 }
