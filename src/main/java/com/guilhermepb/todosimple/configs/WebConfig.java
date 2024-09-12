d2fe74069967e26d545371c180f3619f71b8bebf
Guilherme P Borges da Silva <guilherme.silva645@fatec.sp.gov.br>
2024-09-12 09:16:35 -0300

    Create View

--- src/main/java/com/guilhermepb/todosimple/configs/WebConfig.java
+++ src/main/java/com/guilhermepb/todosimple/configs/WebConfig.java
@@ -10,6 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**");            //Any request made from (/**) will be accepted
     }
+    // free up communication between front and back end
+
 }
